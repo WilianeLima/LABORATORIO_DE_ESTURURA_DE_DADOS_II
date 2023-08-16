bdcf247234c5753631054ee996717705fe0db4df
WilianeLima <142109280+WilianeLima@users.noreply.github.com>
2023-08-15 21:51:28 -0300

    Update Quest4.c

--- Quest4.c
+++ Quest4.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// Função para verificar se um número é perfeito e retornar a soma de seus divisores próprios.
 int isPerfect(int n){
     int soma = 0, i;
+// Loop para iterar através dos possíveis divisores próprios do número.
     for (i = 1; i < n; i++){
         if( n % i == 0){
-            soma+= i;
+            soma+= i; // Adiciona o divisor à soma se for um divisor próprio.
         }
     }
-
+// Verifica se a soma dos divisores próprios é igual ao número original.
     if(soma == n){
-        return soma;
+        return soma; // Retorna a soma se o número for perfeito.
     } 
+    
 }
 
 int main(){
@@ -20,7 +22,7 @@ int n, r;
 printf("\nInsira um número: ");
 scanf("%i", &n);
 
-r = isPerfect(n);
+r = isPerfect(n);  // Chama a função isPerfect para verificar se o número é perfeito.
 
 if(r == n){
     printf("\n%i é um número perfeito", n);
