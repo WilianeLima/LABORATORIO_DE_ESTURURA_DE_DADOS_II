3c1e717b79e768d92f8b0c4648134d33b2cb8235
WilianeLima <142109280+WilianeLima@users.noreply.github.com>
2023-08-15 22:00:33 -0300

    Update Quest7.c

--- Quest7.c
+++ Quest7.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Função recursiva f que recebe dois argumentos inteiros m e n.
+
 f(int m, int n){
+     // Caso base: Quando n chega a 1, retorna m + 1.
     if (n == 1){
         return m + 1;
+        // Caso base: Quando m chega a 1, retorna n + 1.
     } else if(m == 1){
         return n + 1;
+         // Caso geral: Chama recursivamente a função para f(m, n-1) e f(m-1, n) e retorna a soma.
     } else if (m > 1 && n > 1){
         return f(m, n - 1) + f(m-1, n);
     }
